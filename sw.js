3fa7bf47f2850b3c6268af30ac64af678c08483d
bmd <bmdgorsel@gmail.com>
2026-08-04 22:04:33 +0300

    Surum 19: undefined veriyi temizle - senkron cokme duzeltmesi

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Şok Nokta - çevrimdışı önbellek (service worker)
-var CACHE = 'sok-nokta-v18';
+var CACHE = 'sok-nokta-v19';
 var ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', function(e) {
