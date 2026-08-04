a346e79e103c6bfa6b1f764d88bc7a0f7c06aff6
furkanonder-collab <furkanonder@gmail.com>
2026-08-04 19:47:33 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Şok Nokta - çevrimdışı önbellek (service worker)
+var CACHE = 'sok-nokta-v16';
+var ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); })
+            .then(function() { return self.skipWaiting(); })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(keys.map(function(k) {
+                if (k !== CACHE) return caches.delete(k);
+            }));
+        }).then(function() { return self.clients.claim(); })
+    );
+});
+
+// Önce internet dene, güncel sürümü önbelleğe al; internet yoksa önbellekten ver
+self.addEventListener('fetch', function(e) {
+    if (e.request.method !== 'GET') return;
+    e.respondWith(
+        fetch(e.request).then(function(resp) {
+            var copy = resp.clone();
+            caches.open(CACHE).then(function(c) { c.put(e.request, copy); });
+            return resp;
+        }).catch(function() {
+            return caches.match(e.request).then(function(r) {
+                return r || caches.match('./index.html');
+            });
+        })
+    );
+});
