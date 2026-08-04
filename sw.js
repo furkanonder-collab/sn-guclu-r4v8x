0d6014b8c9203eebe5a8b2df30f693ba09336648
bmd <bmdgorsel@gmail.com>
2026-08-04 22:39:10 +0300

    Surum 21: saat-kaymasi duzeltmesi (pending-push bayragi, timestamp yerine)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Şok Nokta - çevrimdışı önbellek (service worker)
-var CACHE = 'sok-nokta-v20';
+var CACHE = 'sok-nokta-v21';
 var ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', function(e) {
