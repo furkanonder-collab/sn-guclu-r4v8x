9b310da441cbf6803ac3401b4367f72446a6c393
bmd <bmdgorsel@gmail.com>
2026-08-06 14:14:24 +0300

    Surum 29: sekmeye donunce oto-resync (kacirilan silme/degisiklik yakalanir) + id normalize

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Şok Nokta - çevrimdışı önbellek (service worker)
-var CACHE = 'sok-nokta-v28';
+var CACHE = 'sok-nokta-v29';
 var ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', function(e) {
