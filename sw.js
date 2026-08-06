fc9e91aa5244092967dcfb03c9d309ed6602c8b1
bmd <bmdgorsel@gmail.com>
2026-08-06 14:24:53 +0300

    Surum 30: ayarlara Guncelle (zorla en son surum) butonu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Şok Nokta - çevrimdışı önbellek (service worker)
-var CACHE = 'sok-nokta-v29';
+var CACHE = 'sok-nokta-v30';
 var ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', function(e) {
