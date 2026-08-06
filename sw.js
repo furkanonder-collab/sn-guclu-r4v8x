44a6a2c21839ec922a294fa93c16507cbdbfe5e1
bmd <bmdgorsel@gmail.com>
2026-08-06 13:13:48 +0300

    Surum 26: tam ekran resimde Geldi/Gelmedi butonu (resimden cikmadan)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Şok Nokta - çevrimdışı önbellek (service worker)
-var CACHE = 'sok-nokta-v25';
+var CACHE = 'sok-nokta-v26';
 var ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', function(e) {
